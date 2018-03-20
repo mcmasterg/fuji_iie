600bdc9d2b36987227a543d51cf56ba2b64e8fa3
Rick Altherr <kc8apf@kc8apf.net>
2018-03-19 21:25:37 -0700

    Disable generated clock constraints
    Using SCOPED_TO_REF XDC constraints implicitly adds a DONT_TOUCH
    property on the affected cells.  That causes problems with tristate
    nets that cross hierarchy boundaries.
    
    Signed-off-by: Rick Altherr <kc8apf@kc8apf.net>

--- targets/basys3/create_project.tcl
+++ targets/basys3/create_project.tcl
@@ -140,24 +140,6 @@ if {[string equal [get_filesets -quiet constrs_1] ""]} {
 set obj [get_filesets constrs_1]
 
 # Add/Import constrs file and set constrs file properties
-set file "[file normalize "$origin_dir/constraints/apple_iie_pal.xdc"]"
-set file_added [add_files -norecurse -fileset $obj $file]
-set file "$origin_dir/constraints/apple_iie_pal.xdc"
-set file [file normalize $file]
-set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
-set_property -name "file_type" -value "XDC" -objects $file_obj
-set_property -name "scoped_to_ref" -value "AppleIIePAL" -objects $file_obj
-
-# Add/Import constrs file and set constrs file properties
-set file "[file normalize "$origin_dir/constraints/mcl65.xdc"]"
-set file_added [add_files -norecurse -fileset $obj $file]
-set file "$origin_dir/constraints/mcl65.xdc"
-set file [file normalize $file]
-set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
-set_property -name "file_type" -value "XDC" -objects $file_obj
-set_property -name "scoped_to_ref" -value "MCL65" -objects $file_obj
-
-# Add/Import constrs file and set constrs file properties
 set file "[file normalize "$origin_dir/targets/basys3/basys3_pins.xdc"]"
 set file_added [add_files -norecurse -fileset $obj $file]
 set file "$origin_dir/targets/basys3/basys3_pins.xdc"
